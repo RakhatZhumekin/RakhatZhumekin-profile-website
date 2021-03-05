53a283194377a990b21ab38f3e15214c27b5c39b
Rakhat Zhumekin <190103441@stu.sdu.edu.kz>
2021-03-05 18:52:38 +0600

    added BlogController and index, as well as route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Models\Post;
-
+use App\Http\Controllers\BlogController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -38,3 +38,5 @@ Route::get('/post', function() {
     $post = Post::find(1);
     return $post;
 }) -> name('post');
+
+Route::get('/blog', [BlogController::class, 'index']);
